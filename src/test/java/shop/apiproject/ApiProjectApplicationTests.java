f2a571e77dde9689bc508f86ed75a18d76a701da
Lee Sang Won <tkddnjs206@gmail.com>
2022-08-15 23:49:39 +0900

    Initial commit

---
+++ src/test/java/shop/apiproject/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.apiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
