156623e5e54201c00893ad0a40d237211431ecdb
Lee Sang Won <tkddnjs206@gmail.com>
2022-08-27 23:23:11 +0900

    HTML 준비 view 단 준비하기

--- src/main/java/shop/apiproject/controller/MainController.java
+++ src/main/java/shop/apiproject/controller/MainController.java
@@ -1,8 +1,12 @@
 package shop.apiproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+
 public class MainController {
-    
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
