85a1e56be424f73ee07170a74582a76253c71419
Lee Sang Won <tkddnjs206@gmail.com>
2022-08-31 23:30:48 +0900

    ready controller 이번 주엔 repository 부분 완벽히 완성하기!

--- src/main/java/shop/apiproject/controller/MainController.java
+++ src/main/java/shop/apiproject/controller/MainController.java
@@ -1,12 +1,18 @@
 package shop.apiproject.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+@Controller
+@Slf4j
 public class MainController {
     @GetMapping("/")
     public String index(){
         return "index";
     }
+
+
 }
