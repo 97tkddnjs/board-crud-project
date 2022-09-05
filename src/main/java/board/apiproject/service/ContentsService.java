c3703e2500b831eb707a63196cd4a4533f374631
Lee Sang Won <tkddnjs206@gmail.com>
2022-09-05 23:26:16 +0900

    service

--- src/main/java/board/apiproject/service/ContentsService.java
+++ src/main/java/board/apiproject/service/ContentsService.java
@@ -1,4 +1,18 @@
 package board.apiproject.service;
 
+import board.apiproject.Repository.CommentsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ContentsService {
+
+    private final CommentsRepository commentsRepository;
+
+    // service로 만들어야 하는 게 생성 수정 삭제 날짜순 정렬, 공감 많은 수 정렬,
+
+
+
+
 }
