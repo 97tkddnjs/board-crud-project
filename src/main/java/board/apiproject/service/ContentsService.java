93f83403390e4dd491bd0f7ab27b3338b7c0d558
Lee Sang Won <tkddnjs206@gmail.com>
2022-09-09 23:35:20 +0900

    contentsService 기본 툴 완성

--- src/main/java/board/apiproject/service/ContentsService.java
+++ src/main/java/board/apiproject/service/ContentsService.java
@@ -40,12 +40,38 @@ public class ContentsService {
     }
 
     // 게시글 공감수 정렬
+    public List<Contents> retrivalEmpathy(){
+        List<Contents> all = contentsRepository.findAll();
+        Collections.sort(all, (o1, o2)
+                -> (new Integer (o2.getEmpathy()).compareTo(new Integer(o1.getEmpathy() ) ) ));
+        return all;
+    }
 
+    // 게시글 검색
+    //1. title로 조회
+    public List<Contents> titleRetrival(Contents title) {
+        return null;
+    }
+
+    //2. contents 내용으로 조회
+    public List<Contents> contentsRetrival(Contents contents){
+        return null;
+    }
 
     /**
      *  Update
      *  id별로 조회
      * */
     //수정
+    // contents는 내용만 변경 가능~
+    public void contentsUpdate(Contents contents){
 
+    }
+
+
+    // delete 하기
+    public void deleteContents(Contents contents){
+
+
+    }
 }
